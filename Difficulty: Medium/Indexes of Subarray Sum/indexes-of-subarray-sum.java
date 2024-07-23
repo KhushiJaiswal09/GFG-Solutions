8211ba5e2a68648b7c8e8d8db1064158998bd9d6
Khushi Jaiswal <103581045+KhushiJaiswal09@users.noreply.github.com>
2024-07-23 21:37:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -1,39 +1,53 @@
 //{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
 
-class Main{
-	static BufferedReader br;
-    static PrintWriter ot;
-    public static void main(String[] args) throws IOException{
-        
-        br = new BufferedReader(new InputStreamReader(System.in));
-        ot = new PrintWriter(System.out);
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
 
-        int t = Integer.parseInt(br.readLine());
-
-        while(t-->0){
-            
-            String s[] = br.readLine().trim().split(" ");
-            
-            int n = Integer.parseInt(s[0]);
-            int k = Integer.parseInt(s[1]);
-            int a[] = new int[n];
-            s = br.readLine().trim().split(" ");
-            for(int i = 0; i < n; i++)
-                a[i] = Integer.parseInt(s[i]);
-            Solution obj = new Solution();
-            ArrayList<Integer> res = obj.subarraySum(a, n, k);
-            for(int ii = 0;ii<res.size();ii++)
-                ot.print(res.get(ii) + " ");
-            ot.println();
-        }
-        ot.close();
+        return a;
     }
 
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        StringBuilder sb = new StringBuilder();
+        for (int e : a) sb.append(e + " ");
+        System.out.println(sb);
+    }
 }
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String str[] = br.readLine().trim().split(" ");
+
+            int n = Integer.parseInt(str[0]);
+            int s = Integer.parseInt(str[1]);
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(arr, n, s);
+
+            IntArray.print(res);
+        }
+    }
+}
+
 // } Driver Code Ends
+
+
 
 
 
