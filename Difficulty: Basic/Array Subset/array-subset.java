5a4494c9d35cc46ac9ae1a4ea162f8e5a5acbe43
Khushi Jaiswal <103581045+KhushiJaiswal09@users.noreply.github.com>
2024-11-18 12:02:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] a = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                a[i] = Integer.parseInt(numsStr[i]);
+            }
+            line = read.readLine().trim();
+            numsStr = line.split(" ");
+            int[] b = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                b[i] = Integer.parseInt(numsStr[i]);
+            }
+            Solution ob = new Solution();
+            if (ob.isSubset(a, b))
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public boolean isSubset(int a[], int b[]) {
+        // Your code here
+        int i=0, j=0, n=a.length, m=b.length;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        
+        while(i<n && j<m){
+            
+            if(a[i]==b[j]){
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+            
+        }
+        if(j==m){
+            return true;
+        }
+        
+        return false;
+    }
+}
