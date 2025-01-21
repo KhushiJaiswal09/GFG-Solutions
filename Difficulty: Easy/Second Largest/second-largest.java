ba20dad3e5594e20ed146fbe1b528aad75aa4898
Khushi Jaiswal <103581045+KhushiJaiswal09@users.noreply.github.com>
2025-01-21 22:25:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -10,15 +10,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.nextLine());
         while (t-- > 0) {
-            List<Integer> arr = new ArrayList<>();
-            String input = sc.nextLine();
-            Scanner ss = new Scanner(input);
-            while (ss.hasNextInt()) {
-                arr.add(ss.nextInt());
-            }
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
             Solution ob = new Solution();
-            int ans = ob.print2largest(arr);
+            int ans = ob.getSecondLargest(arr);
             System.out.println(ans);
+
+            System.out.println("~");
         }
     }
 }
@@ -29,30 +27,22 @@ public class Main {
 // User function Template for Java
 
 class Solution {
-    public int print2largest(List<Integer> arr) {
+    public int getSecondLargest(int[] arr) {
         // Code Here
-        int max=0;
-        int n=arr.size();
-        for(int i=0; i<n; i++){
-            max=Math.max(max, arr.get(i));
-        }
-        
-        for(int i=0; i<n; i++){
-            int count=0;
-            if(max==arr.get(i)){
-                count++;
-                arr.set(i, 0);
+         int largest = Integer.MIN_VALUE;
+        int slargest = Integer.MIN_VALUE;
+        for(int i:arr){
+            if(i>largest){
+                slargest=largest;
+                largest=i;
             }
-            
-            if (count==2)      return -1;
+            else if(i>slargest&&i!=largest){
+                slargest=i;
+            }
         }
-        
-        max=0;
-        for(int i=0; i<n; i++){
-            max=Math.max(max, arr.get(i));
+        if(slargest==Integer.MIN_VALUE){
+            return -1;
         }
-        
-        return max;
-        
+        return slargest;
     }
 }
